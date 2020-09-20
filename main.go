4c0474baf81d3a9c69741acc6fb8e7dfa6f2baaf
Zee Abrahams <zee.j.abrahams@gmail.com>
2020-09-20 01:17:27 -0400

    Basic sending of startup works

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 	"net"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -19,31 +18,77 @@ func main() {
 	}
 	defer conn.Close()
 
-	buff := new(bytes.Buffer)
-	spew.Dump(buff.String())
-	// buffer for the startup message
-	err = binary.Write(buff, binary.BigEndian, int32(196608)) // protocol version
+	stMsg, err := startupMsg()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	err = sendMsg(conn, stMsg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(conn)
+	resp := make([]byte, 1000)
+	for {
+		size, err := reader.ReadByte()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = io.ReadFull(reader, resp[:int(size)])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("received %x\n", resp[:int(size)])
+	}
+
+}
+
+func startupMsg() (*bytes.Buffer, error) {
+	buff := new(bytes.Buffer)
+	// buffer for the startup message
+	err := binary.Write(buff, binary.BigEndian, int32(196608)) // protocol version
+	if err != nil {
+		return nil, err
+	}
+
 	buff.WriteString("user")
-	// buff.WriteByte(0)
+	buff.WriteByte(0)
 
 	buff.WriteString("zee")
 	buff.WriteByte(0)
 
 	buff.WriteByte(0)
 
-	length := buff.Len() + 4
-	fmt.Println("writing to conn")
-	err = binary.Write(conn, binary.BigEndian, int32(length)) // protocol version
+	len := buff.Len() + 4
+
+	msg := new(bytes.Buffer)
+	err = binary.Write(msg, binary.BigEndian, int32(len))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Println("length written")
-	written, err := io.Copy(conn, buff)
 
-	fmt.Printf("startup message sent: %d bytes", written)
+	_, err = io.Copy(msg, buff)
+	if err != nil {
+		return nil, err
+	}
 
+	return msg, nil
+}
+
+func sendMsg(conn net.Conn, msg *bytes.Buffer) error {
+	writer := bufio.NewWriter(conn)
+	written, err := writer.Write(msg.Bytes())
+	if err != nil {
+		return err
+	}
+	fmt.Printf("message sent: %d bytes\n", written)
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
