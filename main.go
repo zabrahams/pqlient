906e16c7f1769f4c8dc006b1321e9d21453e5955
Zee Abrahams <zee.j.abrahams@gmail.com>
2020-09-20 02:07:48 -0400

    Parses auth response and params

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 	"net"
 )
 
+const (
+	AUTHENTICATION_OK                 = 0
+	AUTHENTICATION_KERBEROS_V5        = 2
+	AUTHENTICATION_CLEARTEXT_PASSWORD = 3
+	AUTHENTICATION_MD5_PASSWORD       = 5
+	// there are lots more but I dont see them coming up.
+)
+
 func main() {
 
 	conn, err := net.Dial("tcp", "localhost:5432")
@@ -29,21 +37,72 @@ func main() {
 	}
 
 	reader := bufio.NewReader(conn)
-	resp := make([]byte, 1000)
 	for {
-		size, err := reader.ReadByte()
+		fmt.Println("reading")
+		respType, err := reader.ReadByte()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		_, err = io.ReadFull(reader, resp[:int(size)])
+		sizeInBytes := make([]byte, 4)
+		_, err = io.ReadFull(reader, sizeInBytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var size int32
+		err = binary.Read(bytes.NewBuffer(sizeInBytes), binary.BigEndian, &size)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("received %x\n", resp[:int(size)])
+		resp := make([]byte, size-4)
+		_, err = io.ReadFull(reader, resp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch respType {
+		case byte('R'):
+			fmt.Println("parsing auth response")
+			err = parseAuthResponse(resp)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case byte('S'):
+			fmt.Println("parsing parameter response")
+			err = parseParameterStatusResponse(resp)
+			if err != nil {
+				log.Fatal(err)
+			}
+		default:
+			log.Fatalf("unknown response type: %c", respType)
+		}
 	}
 
+}
+
+func parseAuthResponse(resp []byte) error {
+	var authRespType int32
+	reader := bytes.NewBuffer(resp[:3])
+	binary.Read(reader, binary.BigEndian, &authRespType)
+	switch authRespType {
+	case AUTHENTICATION_OK:
+		fmt.Println("received AUTHENTICATION OK")
+	case AUTHENTICATION_CLEARTEXT_PASSWORD:
+		fmt.Println("received AUTHENTICATION_CLEARTEXT_PASSWORD")
+	case AUTHENTICATION_KERBEROS_V5:
+		fmt.Println("received AUTHENTICATION_KERBEROS_V5")
+	case AUTHENTICATION_MD5_PASSWORD:
+		fmt.Println("received AUTHENTICATION_MD5_PASSWORD")
+	}
+
+	return nil
+}
+
+func parseParameterStatusResponse(resp []byte) error {
+	paramSlice := bytes.Split(resp, []byte{0})
+	fmt.Printf("PARAM: %s\nVALUE: %s\n\n", string(paramSlice[0]), string(paramSlice[1]))
+	return nil
 }
 
 func startupMsg() (*bytes.Buffer, error) {
